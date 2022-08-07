62f0e8e50d95f464943bec2c0631ca86f34a7a3b
Myles Boone <mylesboone@gmail.com>
2022-08-07 17:33:37 -0400

    Line up return value with old one

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def handler(event=None, context=None):
     chrome.get(url)
     time.sleep(random.uniform(int(event['low_sleep']), int(event['high_sleep'])))
 
-    return chrome.get_screenshot_as_base64()
+    return { 'statusCode': 200, 'body': { 'encoded_image': chrome.get_screenshot_as_base64() } }
