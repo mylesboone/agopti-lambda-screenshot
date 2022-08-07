06c9032e3a3ba0ce7d385b4ac1cb7720bd362bcc
Myles Boone <mylesboone@gmail.com>
2022-08-07 17:29:14 -0400

    Take parameters from event

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from tempfile import mkdtemp
 from selenium.webdriver.common.by import By
-
+import random
+import time
 
 def handler(event=None, context=None):
+    url = event['url']
     options = webdriver.ChromeOptions()
     options.binary_location = '/opt/chrome/chrome'
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280x1696")
+    options.add_argument(f"--window-size={event['x_pixels']}x{event['y_pixels']}")
     options.add_argument("--single-process")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-dev-tools")
@@ -18,7 +20,9 @@ def handler(event=None, context=None):
     options.add_argument(f"--data-path={mkdtemp()}")
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
-    chrome = webdriver.Chrome("/opt/chromedriver",
-                              options=options)
-    chrome.get("https://example.com/")
-    return chrome.find_element(by=By.XPATH, value="//html").text
+    options.add_argument('--hide-scrollbars')
+    chrome = webdriver.Chrome("/opt/chromedriver", options=options)
+    chrome.get(url)
+    time.sleep(random.uniform(int(event['low_sleep']), int(event['high_sleep'])))
+
+    return chrome.get_screenshot_as_base64()
